bde860eabb480682d6eb7c726b7c3bde80d32c81
Dancuo-Lohan <dancuo.lohan@gmail.com>
2021-09-23 19:07:35 +0200

    V1.2.2
    Correction d'une erreur d'affichage sur les écrans 15"
    ->Problème au niveau du CSS

---
+++ public/pages/vote.php
@@ -0,0 +1,4 @@
+<div class="infoContent" style="text-align: center;">
+    <h1 style="position:absolute; left:50%; top: 45%; transform: translate(-50%,-50%);">Page en cours de construction...<br><br>
+    <img width="120px" src="https://www.svgrepo.com/show/192275/construction-warning.svg"></h1>
+</div>
